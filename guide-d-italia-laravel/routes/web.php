e5809b79546fde687ece7cdfb61b64c2a2410aaa
Lavinia1830 <lavinia.baratti@gmail.com>
2025-04-01 16:31:05 +0200

    Profile

--- guide-d-italia-laravel/routes/web.php
+++ guide-d-italia-laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -19,18 +20,18 @@ use Inertia\Inertia;
 
 
 Route::get('/', function () {
-    return Inertia::render('Welcome'/* , [
+    return Inertia::render('Home' , [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
-    ] */);
+    ]);
 });
 
 
-Route::get('/home', function () {
+/* Route::get('/', function () {
     return Inertia::render('Home');
-})->middleware(['auth', 'verified'])->name('home');
+})->middleware(['auth', 'verified'])->name('home'); */
 
 /*Start Valle d'Aosta*/
 
